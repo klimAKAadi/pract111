490bf9c0cea3e252621ccc6fb4998ff39376b60e
klim leushin <klim.leushin@gmail.com>
2022-07-21 15:25:25 +0300

    Добавьте файлы проекта.

---
+++ pract111/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using pract111.Models;
+
+namespace pract111.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+    }
+}
---
+++ pract111/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using pract111.Models;   // пространство имен моделей
+using Microsoft.EntityFrameworkCore; // пространство имен EntityFramework
+using Microsoft.Extensions.Hosting;
+
+namespace pract111
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MobileContext>(options => options.UseSqlServer(connection));
+            services.AddControllersWithViews();
+            services.AddSwaggerGen();
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "pract111");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseSwagger(c => c.SerializeAsV2 = true);
+
+        }
+    }
+}
+
